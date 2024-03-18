807926bd9713739046d70e52cf5a21eea44484e5
Rock Yuren Pang <73901780+rrrrrrockpang@users.noreply.github.com>
2024-03-18 16:53:19 -0700

    batches

--- app/semantic_vector_store.py
+++ app/semantic_vector_store.py
@@ -45,29 +45,65 @@ class SemanticVectorStore():
         # cls_pooling
         return model_output.last_hidden_state[:, 0]
 
+    
+    def batch_embeddings(texts, tokenizer, model, device):
+        encoded_input = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
+        encoded_input = {k: v.to(device) for k, v in encoded_input.items()}
+        with torch.no_grad():
+            model_output = model(**encoded_input)
+        return model_output.last_hidden_state[:, 0].detach().cpu().numpy()
+
     def initiaze_embeddings_dataset(self):
         if os.path.exists(self.path):
             embeddings_dataset = load_from_disk(self.path)
-            if embeddings_dataset.num_rows == len(pd.read_csv(self.vanila_path)):
-                embeddings_dataset.add_faiss_index(column="embeddings")
-                return embeddings_dataset
-
+            embeddings_dataset.add_faiss_index(column="embeddings")
+            return embeddings_dataset
+        
         data = pd.read_csv(self.vanila_path)
-        data['vector_text'] = data.apply(lambda x: "Title: {}\n\nAbstract: {}".format(x['title'], x['gpt_summary']), axis=1)
+        data['vector_text'] = data.apply(lambda x: f"Title: {x['title']}\n\nAbstract: {x['gpt_summary']}", axis=1)
+        texts = data['vector_text'].tolist()
 
-        df = Dataset.from_pandas(data)
-        embeddings_dataset = df.map(
-            lambda x: {"embeddings": self.get_embeddings(x["vector_text"]).detach().cpu().numpy()[0]}
-        )
+        # Process in batches
+        batch_size = 100 
+        embeddings = []
+        for i in range(0, len(texts), batch_size):
+            batch_texts = texts[i:i+batch_size]
+            batch_embeddings = batch_embeddings(batch_texts, self.tokenizer, self.model, self.device)
+            embeddings.append(batch_embeddings)
         
-        if not os.path.exists(self.path):
-            embeddings_dataset.save_to_disk(self.path)
-    
-        if embeddings_dataset.num_rows == len(pd.read_csv(self.vanila_path)):
-            embeddings_dataset.save_to_disk(self.path)
+        embeddings = np.vstack(embeddings)
+        data['embeddings'] = embeddings.tolist()
+        embeddings_dataset = Dataset.from_pandas(data)
         
+        embeddings_dataset.save_to_disk(self.path)
         embeddings_dataset.add_faiss_index(column="embeddings")
         return embeddings_dataset
+
+
+
+    # def initiaze_embeddings_dataset(self):
+    #     if os.path.exists(self.path):
+    #         embeddings_dataset = load_from_disk(self.path)
+    #         if embeddings_dataset.num_rows == len(pd.read_csv(self.vanila_path)):
+    #             embeddings_dataset.add_faiss_index(column="embeddings")
+    #             return embeddings_dataset
+
+    #     data = pd.read_csv(self.vanila_path)
+    #     data['vector_text'] = data.apply(lambda x: "Title: {}\n\nAbstract: {}".format(x['title'], x['gpt_summary']), axis=1)
+
+    #     df = Dataset.from_pandas(data)
+    #     embeddings_dataset = df.map(
+    #         lambda x: {"embeddings": self.get_embeddings(x["vector_text"]).detach().cpu().numpy()[0]}
+    #     )
+        
+    #     if not os.path.exists(self.path):
+    #         embeddings_dataset.save_to_disk(self.path)
+    
+    #     if embeddings_dataset.num_rows == len(pd.read_csv(self.vanila_path)):
+    #         embeddings_dataset.save_to_disk(self.path)
+        
+    #     embeddings_dataset.add_faiss_index(column="embeddings")
+    #     return embeddings_dataset
     
 
     def search(self, query, domain, aspect, k=100):

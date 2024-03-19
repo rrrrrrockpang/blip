5a1e362fd2555d2935270ed9fdd5b4aafa24d63b
Rock Yuren Pang <73901780+rrrrrrockpang@users.noreply.github.com>
2024-03-19 09:46:42 -0700

    add urls

--- blip-react/src/components/ArticleModal/ArticleModal.js
+++ blip-react/src/components/ArticleModal/ArticleModal.js
@@ -8,8 +8,13 @@ function ArticleModal({ isOpen, closeModal }) {
   const [isLoading, setIsLoading] = useState(false);
   const [response, setResponse] = useState(null);
 
+  const samples = [
+    { url: "https://techcrunch.com/2023/05/24/the-surgeon-generals-advisory-on-risks-of-youth-social-media-use-could-shift-the-conversation/", domain: "social_media", label: "Social Media" },
+    { url: "https://www.theguardian.com/world/2015/feb/09/south-korean-womans-hair-eaten-by-robot-vacuum-cleaner-as-she-slept", domain: "robotics", label: "Robotics" },
+    { url: "https://medium.com/@sranciato1/ethical-challenges-in-ar-vr-918f259d0b59", domain: "augmented/virtual_reality", label: "Augmented/Virtual Reality" }
+  ];
+
   const submitArticle = async () => {
-    // Validate the URL
     if (!/^(ftp|http|https):\/\/[^ "]+$/.test(url)) {
       alert("Invalid URL format");
       return;
@@ -20,13 +25,14 @@ function ArticleModal({ isOpen, closeModal }) {
     try {
       const response = await fetch(
         "https://blip.labinthewild.org/api/request",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ url, domain }),
-      });
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ url, domain }),
+        }
+      );
 
       if (response.ok) {
         const res = await response.json();
@@ -36,15 +42,19 @@ function ArticleModal({ isOpen, closeModal }) {
       }
     } catch (error) {
       alert("An error occurred. Please try again.");
+    } finally {
+      setIsLoading(false);
     }
-
-    setIsLoading(false);
   };
 
-  // Reset results when the modal is closed or opened
   useEffect(() => {
     setResponse(null);
   }, [isOpen]);
+
+  const loadSample = (sample) => {
+    setUrl(sample.url);
+    setDomain(sample.domain);
+  };
 
   return (
     <div id="articleModal" className={`modal ${isOpen ? 'is-open' : ''}`}>
@@ -52,6 +62,14 @@ function ArticleModal({ isOpen, closeModal }) {
         <span className="close" onClick={closeModal}>&times;</span>
         <h3>Analyze Your Own Article</h3>
         <p>You can input a URL of an online article and select one of the domains. You'll be able to see the results run by our pipeline.</p>
+        <div>
+          {samples.map((sample, index) => (
+            <button className="btn btn-outline-success" key={index} onClick={() => loadSample(sample)}>
+              Try {sample.label} Sample
+            </button>
+          ))}
+        </div>
+
         <label htmlFor="url">Article URL:</label>
         <input type="text" id="url" name="url" value={url} onChange={(e) => setUrl(e.target.value)} />
         <br />
@@ -63,9 +81,6 @@ function ArticleModal({ isOpen, closeModal }) {
                 <option key={index} value={option.toLowerCase().replace(/ /g, "_")}>{option}</option>
               ))
           }
-          {/* <option value="social media">social media</option>
-          <option value="voice assistant">voice assistant</option>
-          <option value="virtual reality">virtual reality</option> */}
         </select>
         <br />
         <button id="submitButton" className="submit-button" onClick={submitArticle}>Submit</button>
@@ -79,38 +94,3 @@ function ArticleModal({ isOpen, closeModal }) {
 }
 
 export default ArticleModal;
-
-
-// import React, { useState } from 'react';
-// import './ContributeModal.css';
-
-// function ArticleModal({ isOpen, closeModal }) {
-//   const [url, setUrl] = useState("");
-//   const [domain, setDomain] = useState("social media");
-//   console.log("Is the modal open?", isOpen);  // Add this line for debugging
-
-//   return (
-//     <div id="articleModal" className={`modal ${isOpen ? 'is-open' : ''}`}>
-//       <div id="articleModalContent">
-//         <span className="close" onClick={closeModal}>&times;</span>
-//         <h3>Analyze Your Own Article</h3>
-//         <p>You can input an URL of an online article and select one of the domain. You'll be able to see the results run by our pipeline.</p>
-//         <label htmlFor="url">Article URL:</label>
-//         <input type="text" id="url" name="url" value={url} onChange={(e) => setUrl(e.target.value)} />
-//         <br />
-//         <label htmlFor="domain">Domain:</label>
-//         <select id="domain" name="domain" value={domain} onChange={(e) => setDomain(e.target.value)}>
-//           <option value="social media">social media</option>
-//           <option value="voice assistant">voice assistant</option>
-//           <option value="virtual reality">virtual reality</option>
-//         </select>
-//         <br />
-//         <button id="submitButton" className="submit-button" onClick={() => {/* Submit code here */}}>Submit</button>
-//         <br />
-//         <div id="results"></div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ArticleModal;
